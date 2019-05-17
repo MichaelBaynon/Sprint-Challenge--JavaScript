fa9937b9f8cd507a944dfae2af316b569465e9e5
Michael Baynon <baynonmichael@gmail.com>
2019-05-17 19:21:52 -0400

    finalRedo

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,29 +1,48 @@
 // ==== Callbacks ====  
 
 /* Step 1: Create a higher-order function that accepts a callback
-  * Create a higher-order function named consume that can take 3 parameters.
-  * The first two parameters can accept any argument
-  * The last parameter accepts a callback 
-  * In the body of the function return the callback with the two parameters that you created
-*/
+ * Create a higher-order function named consume that can take 3 parameters.
+ * The first two parameters can accept any argument
+ * The last parameter accepts a callback 
+ * In the body of the function return the callback with the two parameters that you created
+ */
 
+function consume(param1, param2, cb) {
+  return cb(param1, param2)
+}
 
 /* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+ * Create a function named add that returns the sum of two numbers
+ * Create a function named multiply that returns the product of two numbers 
+ * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+ */
 
+function add(x, y) {
+  return x + y;
+
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function greeting(first, last) {
+  return (`Hello ${first} ${last}, nice to meet you!`)
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
+
+// closure allows functions to access the outer variables closure is going to allow `nestedfunction()` to access anything outside of it complete its task 
+// sometimes someone coding will ask something of a function that is impossible with only the information 
+// with the function itself so with closrure in mind the function looks elswhere or outside to find the information it needs
 
 // Explanation: 
 
